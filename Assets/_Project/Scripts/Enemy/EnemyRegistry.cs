b7dadfae01f824052bed3958cca6834419851cb0
Jhan Khendrick Perez <jhankhendrickperez@gmail.com>
2026-04-29 20:41:13 +0800

    Utilities: Improved Retargeter system

--- Assets/_Project/Scripts/Enemy/EnemyRegistry.cs
+++
@@ -1,7 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class EnemyRegistry
-{
-    public static readonly List<EnemyPathAgent> Enemies = new();
-}
--- Assets/_Project/Scripts/Enemy/EnemyRetargeter.cs
+++ Assets/_Project/Scripts/Enemy/EnemyRetargeter.cs
@@ -16,19 +16,6 @@ public class EnemyRetargeter : MonoBehaviour
             return null;
         }
 
-        PlayerBase best = null;
-        float bestDistance = float.MaxValue;
-
-        foreach (var playerBase in bases)
-        {
-            float distance = Vector3.Distance(fromPosition, playerBase.transform.position);
-
-            if (!(distance < bestDistance)) continue;
-            
-            bestDistance = distance;
-            best = playerBase;
-        }
-
-        return best;
+        return TargetingUtility.GetClosest(bases, fromPosition);
     }
 }
